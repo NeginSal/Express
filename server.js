ba9f5f3e907806c03613a4c22d34b14bb129af71
NeginSal <neginsaljooghi@gmail.com>
2024-12-11 17:33:53 +0330

    feat:added html pages

--- server.js
+++ server.js
@@ -2,4 +2,12 @@ const express = require('express');
 const app = express();
 
 
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.get('/about', (req, res) => {
+  res.send('About');
+});
+
 app.listen(8000, () => { console.log('server start running on port 8000') });
