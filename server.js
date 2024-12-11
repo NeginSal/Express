db67be1d1d11d0c07dfcc6199c1fdbf7ba2f1407
NeginSal <neginsaljooghi@gmail.com>
2024-12-11 17:47:18 +0330

    feat:added static server on nodejs

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
-
-app.get('/about', (req, res) => {
-  res.send('About');
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(8000, () => { console.log('server start running on port 8000') });
